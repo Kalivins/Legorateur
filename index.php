546bfb5a9aa76745b2b61b3c8d65ce5062cbc3b9
Unknown <kevin.v@codeur.online>
2017-11-03 11:35:29 +0100

    bootstrap and jquery

--- index.php
+++ index.php
@@ -3,20 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>Explorateur de fichiers</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-   
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script> 
-    <script src="js/app.js"></script>
-</body>
-</html>
-<?php
+  <?php
 function scan($dir) {
     if(is_dir($dir)) {
         $files = scandir($dir);
         unset($files[0], $files[1]);
         natcasesort($files);
+        echo '<ul>';
         foreach($files as $f) {
             if(is_dir($dir.$f)) {
                 echo '<li class="folder">'.$f.'</li>';
@@ -25,12 +22,20 @@ function scan($dir) {
                 echo '</ul>';
             }
         }
+        echo '</ul>';
+        echo '<ul>';
           foreach($files as $f) {
             if(is_file($dir.$f)) {
                 echo '<li class="file" rel="'.$dir.$f.'">'.$f.'</li>';
             }
         }
+        echo '</ul>';
     }
 }
 scan('../');
 ?>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script> 
+    <script src="js/app.js"></script>
+</body>
+</html>
