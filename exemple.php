cba1a794b62025ba9f2fab74d605aa18c628aa3d
Romain Tharradin <romain.t@codeur.online>
2017-11-03 10:04:24 +0100

    exemple

---
+++ exemple.php
@@ -0,0 +1,21 @@
+<h1>Titre</h1>
+<?php
+function mkmap($dir){
+    echo "<ul>";   
+    $folder = opendir ($dir);
+   
+    while ($file = readdir ($folder)) {   
+        if ($file != "." && $file != "..") {           
+            $pathfile = $dir.'/'.$file;           
+            echo "<li><a href=$pathfile>$file</a></li>";           
+            if(filetype($pathfile) == 'dir'){               
+                mkmap($pathfile);               
+            }           
+        }       
+    }
+    closedir ($folder);    
+    echo "</ul>";   
+}
+?>
+
+<?php mkmap('.'); ?>
