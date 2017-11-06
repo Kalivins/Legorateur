6e4a370f16e7b3f021c30fdb926da5f2072bb0c3
Unknown <kevin.v@codeur.online>
2017-11-06 09:08:45 +0100

    fonction de sélection/dblclick

--- index.php
+++ index.php
@@ -8,18 +8,18 @@
 </head>
 <body>
   <?php
-function arbo($dir) {                                   //on declare la fonction qui permet de recuperer l'arborescence du dossier
-    if(is_dir($dir)) {                                 // on vérifie que le dossier existe
-        $files = scandir($dir);                  // on stocke ds la variable files la liste des fichiers present dans le dossier
-        unset($files[0], $files[1]);          // on retire les points du tableaux
-        natcasesort($files);                                 // permet de classer les dossiers ds le tableau par nom
-        echo '<ul>';                                     
-        foreach($files as $f) {                          // pertmet de retirer les valeurs du tableau files ds la variable F      
+function arbo($dir) {                                   //On declare la fonction qui permet de recuperer l'arborescence du dossier
+    if(is_dir($dir)) {                                 // On vérifie que le dossier existe
+        $files = scandir($dir);                  // On stocke ds la variable files la liste des fichiers present dans le dossier
+        unset($files[0], $files[1]);          // On retire les points du tableaux
+        natcasesort($files);                    // permet de classer les dossiers ds le tableau par nom         
+        echo "<ul>";
+        foreach($files as $f) {                          // permet de retirer les valeurs du tableau files ds la variable F      
             if(is_dir($dir.$f)) {                        //on verifie que le chemin du dossier existe
                 echo '<li class="folder">'.$f.'</li>';           //on liste nos dossier
                 echo '<ul class="tree">';                          //liste les fichiers
-                arbo($dir.$f."/");                        // on appelle la fonction SCAN  pour pouvoir acceder au dossier
-                echo '</ul>';                              
+                arbo($dir.$f."/");                        // on appelle la fonction SCAN  pour pouvoir acceder au dossier  
+                echo "</ul>";
             }
         }
         echo '</ul>';                              
@@ -32,7 +32,7 @@ function arbo($dir) {                                   //on declare la fonction
         echo '</ul>';
     }
 }
-arbo('../');                                       // 
+arbo('../');                                       
 ?>
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.js"></script> 
--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-jQuery(function($){
+$(document).ready(function($){
     $("ul.tree").hide();
-    $("li.folder").click(function () {
+    $("li.folder").click(function() {
+       $(this).each(function () {
+          if (this.style.backgroundColor !== ""){
+              this.style.backgroundColor = "";
+          } else {
+              this.style.backgroundColor = "grey";
+          }
+       }); 
+        });
+    $("li.folder").dblclick(function () {
         $(this).next("ul").toggle("fast");
     });
 });
