37a84706d2dfda44bf7a8dec983897aa565586ab
Unknown <kevin.v@codeur.online>
2017-11-06 11:13:54 +0100

    hover function

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function($){
     $("ul.tree").hide();
-    $("li.folder").click(function() {
-       $(this).each(function () {
-          if (this.style.backgroundColor !== ""){
-              this.style.backgroundColor = "";
-          } else {
-              this.style.backgroundColor = "grey";
-          }
-       }); 
+    $("li.folder").hover(function(){
+        $(this).each(function (){
+            if(this.style.backgroundColor !== ""){
+                $(this).css("background", "");
+            } else {
+                $(this).css("background", "grey");
+            }
         });
+    });
     $("li.folder").dblclick(function () {
         $(this).next("ul").toggle("fast");
     });
