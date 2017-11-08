6ea01bb1b717e12c61240c9fe691d24c44d801b1
Unknown <kevin.v@codeur.online>
2017-11-08 11:33:49 +0100

    bouton retour, fix generation

--- index.php
+++ index.php
@@ -14,10 +14,10 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 pull-left">
-					<img class="logo" src="images/legorateurC.svg" alt="logo">
+					<img class="logo animated bounceIn" src="images/legorateurC.svg" alt="logo">
 				</div>
 				<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-					<img class="titre" src="images/logotexte.png" alt="Legorateur">
+					<img class="titre animated bounceIn" src="images/logotexte.png" alt="Legorateur">
 				</div>
 			</div>
 		</div>
@@ -26,7 +26,9 @@
 
 	<main class="container-fluid">
 		<div class="container">
-		<div class="row explore"></div>
+		<div class="row explore">
+		    
+		</div>
 	</div>
 	</main>
 
@@ -34,20 +36,20 @@
 	<footer class="foot container-fluid">
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 crea">
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 crea animated bounceIn">
 					<img src="images/rom.svg">
-					<figcaption>Roro</figcaption>
+					<figcaption class="animated bounceIn">Roro</figcaption>
 				</div>
-				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 crea">
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 crea animated bounceIn">
 					<img src="images/toin.svg">
-					<figcaption>Toinou</figcaption>
+					<figcaption class="animated bounceIn">Toinou</figcaption>
 				</div>
-				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 crea">
-					<img src="images/lilyh.svg"><figcaption>Lily</figcaption>
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 crea animated bounceIn">
+					<img src="images/lilyh.svg"><figcaption class="animated bounceIn">Lily</figcaption>
 				</div>
-				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 crea">
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 crea animated bounceIn">
 					<img src="images/kev.svg">
-					<figcaption>Kev1</figcaption>
+					<figcaption class="animated bounceIn">Kev1</figcaption>
 				</div>
 			</div>
 
--- js/app.js
+++ js/app.js
@@ -7,25 +7,32 @@ $(document).ready(function($){
         type: "post",
         dataType: "json",
         data: {toinou: path},
-
         success: function (data){
-            console.log(data[5]);
+             if(!path == "") {
+                var pathBack = path.split("\\");
+                var lastPath = pathBack.pop();
+                var link = pathBack.join('/');
+                console.log(link);
+                $('.explore').append("<a class='back' data='"+link+"'>Retour</a>");
+                 
+            }
             $(".explore").append('<ul class="row test"></ul>');
             data.forEach(function (index){
             if(index.isDirectory){
-             $(".test").append("<div data='"+index.pathname+"' class='col-md-2 col-lg-2 folder item'><img src='images/folderR.svg'><li>"+index.filename+"</li></div>");   
+             $(".test").append("<div data='"+index.pathname+"' class='col-md-2 col-lg-2 folder item'><img src='images/folderR.svg'><li>"+index.filename+"</li><a href='#' class='liens_invisible'>Ouvrir</a> </div>");
             } 
             
             if(index.isFile){
-                $(".test").append("<div data='"+index.pathname+"' class='col-md-2 col-lg-2 folder itemF'><img class='sv' src='images/fileR.svg'><p class='extension'>"+index.extension+"</p><li class='file'>"+index.filename+"</li><br><a href='"+index.pathname+"' download='"+index.filename+"'>Télécharger</a> <a href='"+index.pathname+"'>Ouvrir</a></div>");
+                $(".test").append("<div data='"+index.pathname+"' class='col-md-2 col-lg-2 folder itemF'><img class='sv' src='images/fileR.svg'><p class='extension'>"+index.extension+"</p><li class='file'>"+index.filename+"</li><div class='lien'><a href='"+index.pathname+"' download='"+index.filename+"'>Télécharger</a> <a target='_blank' href='"+index.pathname+"'>Ouvrir</a></div></div>");
             }
         });
+            $('.folder').fadeIn(500);
              $(".item").hover(function(){
         $(this).each(function (){
             if(this.style.backgroundColor !== ""){
                 $(this).css("background", "");
             } else {
-                $(this).css("background", "lightgray");
+                $(this).css("background", "rgba(0,0,0,0.2)");
             }
         });
     });
@@ -34,20 +41,23 @@ $(document).ready(function($){
             if(this.style.backgroundColor !== ""){
                 $(this).css("background", "");
             } else {
-                $(this).css("background", "silver");
+                $(this).css("background", "rgba(0,0,0,0.3)");
             }
         });
     });
+            $(".back").click(function(){
+                var rLink = $(this).attr('data');
+                $('.explore').append(" → "+lastPath+"");
+               $(".explore").empty();
+                generation(rLink);
+            });
             $(".item").dblclick(function(){
                 
                 var name = $(this).text();
-                console.log(name);
-
                 var path = $(this).attr("data");
-
+                    console.log(path);
                $(".explore").empty();
                generation(path);
-                
                 });
             }
         });
